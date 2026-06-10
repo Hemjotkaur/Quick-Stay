b59f253b9837712440a31e8c1743534cc8f1c5f1
Hemjotkaur <hemjotkaur786@gmail.com>
2026-06-10 15:50:36 +0530

    feat: Add room listing, owner rooms, and availability toggle APIs

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,4 +1,7 @@
+import Room from "../models/Room.js";
+
 //api to create a new room for a hotel
+
 export const createRoom = async (req, res) => {
   try {
     const { roomType, pricePerNight, amenities } = req.body;
@@ -29,10 +32,45 @@ export const createRoom = async (req, res) => {
 };
 
 //api to get all rooms
-// export const createRoom = async (req, res) => {};
+export const getRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({ isAvailable: true })
+      .populate({
+        path: "hotel",
+        populate: {
+          path: "owner",
+          select: "image",
+        },
+      })
+      .sort({ createdAt: -1 });
+    res.json({ success: true, rooms });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-// //api to get all rooms for a specific hotel
-// export const createRoom = async (req, res) => {};
+//api to get all rooms for a specific hotel
+export const getOwnerRooms = async (req, res) => {
+  try {
+    const hotelData = await Hotel({ owner: req.auth.userId });
+    const rooms = await Room.find({ hotel: hotelData._id.toString() }).populate(
+      "hotel",
+    );
+    res.json({ successs: true, rooms });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-// //api to toggle availability of a room
-// export const createRoom = async (req, res) => {};
+//api to toggle availability of a room
+export const toggleRoomAvailability = async (req, res) => {
+  try {
+    const { roomId } = req.body;
+    const roomData = await Room.findById(roomId);
+    roomData.isAvailable = !roomData.isAvailable;
+    await roomData.save();
+    res.json({ success: true, message: "Room availability Updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
