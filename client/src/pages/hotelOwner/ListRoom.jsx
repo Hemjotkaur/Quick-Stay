bbaeab08d23bce4f082d7455e4f576d134538bff
Hemjotkaur <hemjotkaur786@gmail.com>
2026-06-13 11:48:06 +0530

    feat(room): implement owner room listing and availability toggle

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
 import { roomsDummyData } from "../../assets/assets";
 import Title from "../../components/Title";
+import toast from "react-hot-toast";
+import { useAppContext } from "../../context/AppContext";
+import { useEffect } from "react";
 
 const ListRoom = () => {
   const [rooms, setRooms] = useState(roomsDummyData);
+  const { axios, getToken, user } = useAppContext();
+
+  //Fetch Rooms of the Hotel Owner
+  const fetchRooms = async () => {
+    console.log("hieeeeeeeee");
+    try {
+      const { data } = await axios.get("/api/rooms/owner", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      console.log("roomdata", data);
+      if (data.success) {
+        setRooms(data.rooms);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  //toggle availability of the room
+  const toggleAvailability = async (roomId) => {
+    const { data } = await axios.post(
+      "api/rooms/toggle-availability",
+      { roomId },
+      {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      },
+    );
+    if (data.success) {
+      toast.success(data.message);
+      fetchRooms();
+    } else {
+      toast.error(data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchRooms();
+    }
+  }, [user]);
 
   return (
     <div>
@@ -46,6 +91,7 @@ const ListRoom = () => {
                 <td className="py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center">
                   <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
                     <input
+                      onChange={() => toggleAvailability(item._id)}
                       type="checkbox"
                       className="sr-only peer"
                       checked={item.isAvailable}
