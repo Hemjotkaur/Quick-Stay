d98e95b787a13fb606bc67f156fce77ee473be6b
Hemjotkaur <hemjotkaur786@gmail.com>
2026-06-17 10:59:26 +0530

    refactor: fix recent search state and recommended hotels logic

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     navigate(`/rooms?destination=${destination}`);
 
     // call api to save recent searched citty
-    await axios.post(
+     await axios.post(
       "/api/user/store-recent-search",
       { recentSearchedCity: destination },
       { headers: { Authorization: `Bearer ${await getToken()}` } },
@@ -28,7 +28,6 @@ const Hero = () => {
     });
   };
 
-  console.log("Sss", searchedCities);
 
   return (
     <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url('/src/assets/heroImage.png')]  bg-no-repeat bg-cover bg-center h-screen ">
