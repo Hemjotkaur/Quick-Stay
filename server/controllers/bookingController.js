f0a76acbb2088c217ce1a9ae808bf4b39de706f7
Hemjotkaur <hemjotkaur786@gmail.com>
2026-06-17 19:30:34 +0530

    feat: add booking confirmation email with nodemailer

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,6 @@
 import Room from "../models/Room.js";
+import Booking from "../models/Booking.js";
+import transporter from "../configs/nodemailer.js";
 
 //function to Check availability of Room
 const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
@@ -25,6 +27,7 @@ export const checkAvailabilityAPI = async (req, res) => {
       checkOutDate,
       room,
     });
+    console.log("isavailable", isAvailable);
     res.json({ success: true, isAvailable });
   } catch (error) {
     res.json({ success: false, message: error.message });
@@ -69,6 +72,45 @@ export const createBooking = async (req, res) => {
       checkOutDate,
       totalPrice,
     });
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: req.user.email,
+      subject: "Hotel Booking Details",
+      html: `
+       <h2>Your Booking Details</h2>
+
+<p>Dear ${req.user.username},</p>
+
+<p>Thank you for your booking! Here are your details:</p>
+
+<ul>
+  <li>
+    <strong>Booking ID:</strong> ${booking._id}
+  </li>
+  <li>
+    <strong>Hotel Name:</strong> ${roomData.hotel.name}
+  </li>
+  <li>
+    <strong>Location:</strong> ${roomData.hotel.address}
+  </li>
+  <li>
+    <strong>Date:</strong> ${booking.checkInDate.toDateString()}
+  </li>
+  <li>
+    <strong>Booking Amount:</strong>
+    ${process.env.CURRENCY || "$"} ${booking.totalPrice} /night
+  </li>
+</ul>
+
+<p>We look forward to welcoming you!</p>
+
+<p>If you need to make any changes, feel free to contact us.</p>
+      `
+    };
+
+    await transporter.sendMail(mailOptions);
+
     res.json({ success: true, message: "Booking created successfully" });
   } catch (error) {
     console.log(error);
